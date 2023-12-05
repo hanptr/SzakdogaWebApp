9524f995d8e578f38cea2d0e860f11e2ab7c852b
HanczikPeter <92090311+hanptr@users.noreply.github.com>
2023-12-05 14:07:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import numpy as np
+import streamlit as st
 import pickle
 
 transformer_model=pickle.load(open('transfromer_model.sav', 'rb'))
- =pickle.load(open('transfromer_model.sav', 'rb'))
+bidirectional_model==pickle.load(open('transfromer_model.sav', 'rb'))
+ 
+ with st.sidebar:
+    
+    selected = option_menu('Running intensity classification',
+                          
+                          ['Data analytics',
+                           'Classification',
+                           'Upload CSV'],
+                          icons=['clipboard_data','heart','cloud_upload'],
+                          default_index=0)
+    
+    if (selected == 'Data analytics'):
+        # page title
+        st.title('Analyzing the data')
+        
+    if (selected == 'Data Classification'):
+        # page title
+        st.title('Classifying the data')
+        
+    if (selected == 'Upload CSV'):
+        # page title
+        st.title('Upload the desired CSV')
