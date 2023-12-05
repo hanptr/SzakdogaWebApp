7c57e1ffc801bfb3800c1b77d0fb67ac98a115bd
HanczikPeter <92090311+hanptr@users.noreply.github.com>
2023-12-05 13:26:05 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pickle
+
+transformer_model=pickle.load(open('transfromer_model.sav', 'rb'))
+ =pickle.load(open('transfromer_model.sav', 'rb'))
