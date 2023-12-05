abe0b4d13764a246ba6203dea2e5f01345dfa38c
HanczikPeter <92090311+hanptr@users.noreply.github.com>
2023-12-05 14:13:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 import numpy as np
 import streamlit as st
 import pickle
+from streamlit_option_menu import option_menu
 
 transformer_model=pickle.load(open('transfromer_model.sav', 'rb'))
-bidirectional_model==pickle.load(open('transfromer_model.sav', 'rb'))
+bidirectional_model==pickle.load(open('bidirectional_model.sav', 'rb'))
  
- with st.sidebar:
-    
-    selected = option_menu('Running intensity classification',
-                          
-                          ['Data analytics',
-                           'Classification',
-                           'Upload CSV'],
-                          icons=['clipboard_data','heart','cloud_upload'],
-                          default_index=0)
-    
-    if (selected == 'Data analytics'):
-        # page title
-        st.title('Analyzing the data')
-        
-    if (selected == 'Data Classification'):
-        # page title
-        st.title('Classifying the data')
-        
-    if (selected == 'Upload CSV'):
-        # page title
-        st.title('Upload the desired CSV')
+
+with st.sidebar:
+   
+   selected = option_menu('Running intensity classification',
+                         
+                         ['Data analytics',
+                          'Classification',
+                          'Upload CSV'],
+                         icons=['clipboard_data','heart','cloud_upload'],
+                         default_index=0)
+   
+   if (selected == 'Data analytics'):
+       # page title
+       st.title('Analyzing the data')
+       
+   if (selected == 'Data Classification'):
+       # page title
+       st.title('Classifying the data')
+       
+   if (selected == 'Upload CSV'):
+       # page title
+       st.title('Upload the desired CSV')
