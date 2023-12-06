bab1eab256e47c8c8b0d545c326528c148c3f8cd
HanczikPeter <92090311+hanptr@users.noreply.github.com>
2023-12-06 12:03:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,34 +12,30 @@ transformer_model = load_model('./transformer_model.h5')
 bidirectional_model = load_model('./bidirectional_model.h5')
 
 with st.sidebar:
-    
-    
     selected = option_menu('Running intensity classification',
-                         
-                         ['Data analytics',
-                          'Classification',
-                          'Upload CSV'],
-                         icons=['task','heart','upload'],
-                         default_index=0)
-    if (selected == 'Data analytics'):
+                           ['Data analytics',
+                            'Classification',
+                            'Upload CSV'],
+                           icons=['task', 'heart', 'upload'],
+                           default_index=0)
+    if selected == 'Data analytics':
         # page title
         st.title('Analyzing the data')
-       
-    if (selected == 'Data Classification'):
+
+    if selected == 'Data Classification':
         # page title
         st.title('Classifying the data')
-       
-    if (selected == 'Upload CSV'):
-               
+
+    if selected == 'Upload CSV':
         # page title
         st.title('Upload the desired CSV')
-       
-        uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
-        
-        if uploaded_file is not None:
-        st.write("File Uploaded Successfully!")
 
-        # Display uploaded file as a DataFrame
-        df = pd.read_csv(uploaded_file)
-        st.write("### Uploaded Data:")
-        st.write(df)
+        uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+
+        if uploaded_file is not None:
+            st.write("File Uploaded Successfully!")
+
+            # Display uploaded file as a DataFrame
+            df = pd.read_csv(uploaded_file)
+            st.write("### Uploaded Data:")
+            st.write(df)
